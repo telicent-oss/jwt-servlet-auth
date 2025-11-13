d2a3d7b3cf33a81c678865aea3288f9a7da3a79c
Rob Vesse <rob.vesse@telicent.io>
2025-11-13 14:59:50 +0000

    Add OpenID Connect configuration discovery
    This commit adds support for OpenID Connect configuration discovery.
    This means that rather than having to know the JWKS URL up front you can
    now just supply the OpenID Connect provider URL and a new
    OpenIdConnectDiscoveryLocator will obtain the configuration and find the
    JWKS URL automatically the first time it needs to do so.

---
+++ jwt-servlet-auth-core/src/test/java/io/telicent/servlet/auth/jwt/verification/jwks/TestOpenIdConnectDiscoveryLocator.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.servlet.auth.jwt.verification.jwks;
+
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.security.InvalidKeyException;
+import io.jsonwebtoken.security.Jwk;
+import io.jsonwebtoken.security.JwkSet;
+import io.telicent.servlet.auth.jwt.verification.TestKeyUtils;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.mockito.Mockito.when;
+
+public class TestOpenIdConnectDiscoveryLocator {
+
+    private static final Random RANDOM = new Random();
+    private static final AtomicInteger TEST_PORT = new AtomicInteger(52000 + RANDOM.nextInt(50));
+
+    private JwkSet jwks;
+    private OidcServer server;
+
+    @BeforeClass
+    public void setup() throws Exception {
+        this.jwks = TestKeyUtils.buildComplexJwks();
+        this.server = new OidcServer(TEST_PORT.getAndIncrement(), this.jwks);
+        this.server.start();
+    }
+
+    @AfterMethod
+    public void cleanup() {
+        this.server.resetDiscoveryRequestsCount();
+    }
+
+    @AfterClass
+    public void teardown() throws Exception {
+        this.server.stop();
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = ".*cannot be negative")
+    public void givenNegativeRetryInterval_whenCreatingLocator_thenIllegalArgument() {
+        // Given, When and Then
+        new OpenIdConnectDiscoveryLocator(URI.create(this.server.getConfigurationUrl()), Duration.ofSeconds(-3));
+    }
+
+    @Test
+    public void givenCompliantDiscoveryUrl_whenUsingLocator_thenKeyLocated_andSubsequentLocationDoesNotRequireDiscovery() throws
+            Exception {
+        // Given
+        OpenIdConnectDiscoveryLocator locator =
+                new OpenIdConnectDiscoveryLocator(URI.create(this.server.getConfigurationUrl()));
+        JwsHeader header = Mockito.mock(JwsHeader.class);
+        String keyId = this.jwks.getKeys().stream().findFirst().map(Jwk::getId).orElse(null);
+        Assert.assertNotNull(keyId);
+        when(header.getKeyId()).thenReturn(keyId);
+        Assert.assertEquals(server.getDiscoveryRequestsCount(), 0);
+
+        // When and Then
+        Assert.assertNotNull(locator.locate(header));
+        Assert.assertEquals(server.getDiscoveryRequestsCount(), 1);
+
+        // And
+        Assert.assertNotNull(locator.locate(header));
+        Assert.assertEquals(server.getDiscoveryRequestsCount(), 1);
+    }
+
+    @Test
+    public void givenNonStandardDiscoveryUrl_whenUsingLocator_thenWarningIssued() {
+        // Given and When
+        OpenIdConnectDiscoveryLocator locator =
+                new OpenIdConnectDiscoveryLocator(URI.create(this.server.getNonStandardConfigurationUrl()));
+        JwsHeader header = Mockito.mock(JwsHeader.class);
+        String keyId = this.jwks.getKeys().stream().findFirst().map(Jwk::getId).orElse(null);
+        Assert.assertNotNull(keyId);
+        when(header.getKeyId()).thenReturn(keyId);
+
+        // Then
+        Assert.assertNotNull(locator.locate(header));
+    }
+
+    @Test(expectedExceptions = InvalidKeyException.class, expectedExceptionsMessageRegExp = "Unable to resolve JWKS.*")
+    public void givenDiscoveryUrlWhereConfigurationDoesNotContainJwksUrl_whenUsingLocator_thenInvalidKeyException() {
+        // Given and When
+        OpenIdConnectDiscoveryLocator locator =
+                new OpenIdConnectDiscoveryLocator(URI.create(this.server.getEmptyConfigurationUrl()));
+        JwsHeader header = Mockito.mock(JwsHeader.class);
+        String keyId = this.jwks.getKeys().stream().findFirst().map(Jwk::getId).orElse(null);
+        Assert.assertNotNull(keyId);
+        when(header.getKeyId()).thenReturn(keyId);
+
+        // Then
+        locator.locate(header);
+    }
+
+    @Test(expectedExceptions = InvalidKeyException.class, expectedExceptionsMessageRegExp = "Unable to resolve JWKS.*")
+    public void givenDiscoveryUrlWithNon200Response_whenUsingLocator_thenInvalidKeyException() {
+        // Given and When
+        OpenIdConnectDiscoveryLocator locator =
+                new OpenIdConnectDiscoveryLocator(URI.create(this.server.getNotFoundConfigurationUrl()));
+        JwsHeader header = Mockito.mock(JwsHeader.class);
+        String keyId = this.jwks.getKeys().stream().findFirst().map(Jwk::getId).orElse(null);
+        Assert.assertNotNull(keyId);
+        when(header.getKeyId()).thenReturn(keyId);
+
+        // Then
+        locator.locate(header);
+    }
+}
